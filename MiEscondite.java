e36d59dec532237412b0c36a6d1702830dfd6fd0
Nadia <nadieta@gmail.com>
2017-11-04 15:40:16 +0100

    Basic FOR loop example

---
+++ MiEscondite.java
@@ -0,0 +1,12 @@
+public class MiEscondite {
+    public static void main(String[] args){
+	
+         //int niñosJugadores = 6;
+         //int niñosEncontrados = 0;
+
+         for(int niñosEncontrados=0; niñosEncontrados<6; niñosEncontrados++){
+		 
+              System.out.println("Una vuelta más. Niños encontrados: " + niñosEncontrados);
+         }
+    }
+}
