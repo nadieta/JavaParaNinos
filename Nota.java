5dd40faa60e0e70eb69a33e8212133dea8d0573e
Nadia <nadieta@gmail.com>
2017-11-04 15:37:50 +0100

    Basic if/then/else statement

---
+++ Nota.java
@@ -0,0 +1,19 @@
+
+public class Nota{
+
+	
+	public static void main(String []args){
+		
+        int notaExamen = 8;
+
+
+        if (notaExamen>=8) {
+            System.out.println("Enhorabuena, has aprobado con nota.");
+        } else if (notaExamen>= 5) {
+            System.out.println("Has aprobado el examen.");
+        } else {
+           System.out.println("Lo siento, has suspendido el examen.");
+        } 
+        
+    }
+}
